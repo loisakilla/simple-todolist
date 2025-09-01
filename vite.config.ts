759d8e5c4c2dcd857b80c46ae0cf9268e023b8ab
George <kataeffg@yandex.ru>
2025-09-01 20:57:25 +0300

    Simple todo list

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 
 import { defineConfig } from "vite";
+// @ts-ignore
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   test: {
